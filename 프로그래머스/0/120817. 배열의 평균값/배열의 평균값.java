611d3d28f8af57f7aea0a324699eae2bd56a2f79
Shon Juseong <sow7722@gmail.com>
2025-05-16 15:25:05 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,34 +1,9 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        for (int num : numbers){answer += num;}
-                
-        return answer / numbers.length;
+        
+        for(int i : numbers){
+            answer += i;}
+        return answer/numbers.length;
     }
 }
-
-//-----------------------------------------------------------------------------------
-import java.util.Arrays;
-
-class Solution {
-    public double solution(int[] numbers) {
-        return Arrays.stream(numbers).average().orElse(0);
-    }
-}
-//-----------------------------------------------------------------------------------
-class Solution {
-    public double solution(int[] numbers) {
-        double answer = 0;
-        for(int i = 0; i < numbers.length; i++) {
-            answer += numbers[i];
-        }
-
-        answer /= numbers.length;
-        //answer = answer / numbers.lengrh;
-        return answer;
-    }
-}
-
-
-//복합 대입 연산자 
-//for문 // for each
