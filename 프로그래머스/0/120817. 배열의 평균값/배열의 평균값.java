73c5ca3ee183262a0fdec100dd43973dc6341a14
Shon Juseong <sow7722@gmail.com>
2025-05-16 14:35:16 +0900

    Update 배열의 평균값.java

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -6,3 +6,29 @@ class Solution {
         return answer / numbers.length;
     }
 }
+
+//-----------------------------------------------------------------------------------
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+}
+//-----------------------------------------------------------------------------------
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+
+        answer /= numbers.length;
+        //answer = answer / numbers.lengrh;
+        return answer;
+    }
+}
+
+
+//복합 대입 연산자 
+//for문 // for each
