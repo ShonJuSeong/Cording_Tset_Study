f29a0de1b78854b9c1dc8a16eceace757e1dc8c0
Shon Juseong <sow7722@gmail.com>
2025-05-16 10:21:17 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -5,3 +5,13 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        double value = (double)num1 / num2 * 1000;
+        //(double)num1: num1을 double 타입으로 강제 형변환
+        answer = (int)value;
+        return answer;
+    }
+}
